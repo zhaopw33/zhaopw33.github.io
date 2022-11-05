c8a6c3917a8f9cc9d58148612406e1b2db3dd533
zhaopw33 <69562724+zhaopw33@users.noreply.github.com>
2022-11-05 12:14:18 +0800

    Update testexec.php

--- docs/testexec.php
+++ docs/testexec.php
@@ -1,4 +1,4 @@
 <?php
-shell_exec("/var/www/html/testscript/test.sh");
-header('Location: http://192.168.1.222/testscript/index.html?success=true');
+shell_exec("test.sh");
+header('Location: http://192.168.1.222/index.html?success=true');
 ?>
